80732128aa28200eec8bedd46997f980e614d31b
Joachim Byrnes-Shay <me@joachimbyrnesshay.com>
2023-01-11 03:52:51 -0500

    simplified component structure and folder structure for Charting

--- src/components/Charting/BarChart/BarChart.js
+++ src/components/Charting/BarChart/BarChart.js
@@ -1,31 +1,35 @@
 import ChartsUtils from "../AllCharts/utils-allChartsDisplay.js";
-import { ChartModalHandlers} from "./utils-barChart.js";
+import { ChartModalHandlers } from "./utils-barChart.js";
 
 import Modal from "../Modal/Modal.js";
-import BarChartTitle from "../BarChartTitle/BarChartTitle";
+import BarChartTitle from "./BarChartTitle";
 
- function BarChart(props) {
+function BarChart(props) {
     // const ix = props.ix;
     // const isSmallScreen = props.isSmallScreen; ?????
     // const modalNowState = props.modalNowState;
     // const chartInfo = props.chartInfo;
     // const currencyInfo = props.currencyInfo;
     // const currencySelections = props.currencySelections;
-    const onClickHandler = e => ChartModalHandlers.showModal(e,props.ix,props.modalNowState);
-    const onMouseLeaveHandler = e => ChartModalHandlers.hideModal(e,props.ix, props.modalNowState);
-    const chartsOrientation = ChartsUtils.getChartsOrientation(props.isSmallScreen);
-    const stylePerScreenSize = {[chartsOrientation]:String(props.chartInfo.size) + "%"}
+    const onClickHandler = (e) =>
+        ChartModalHandlers.showModal(e, props.ix, props.modalNowState);
+    const onMouseLeaveHandler = (e) =>
+        ChartModalHandlers.hideModal(e, props.ix, props.modalNowState);
+    const chartsOrientation = ChartsUtils.getChartsOrientation(
+        props.isSmallScreen
+    );
+    const stylePerScreenSize = {
+        [chartsOrientation]: String(props.chartInfo.size) + "%",
+    };
     return (
         <div key={props.ix} className={`ChartContent-barChartContainer`}>
-        <div
+            <div
                 style={stylePerScreenSize}
                 className={`ChartContent-barChart`}
                 onClick={onClickHandler}
                 onMouseLeave={onMouseLeaveHandler}
             >
-                <BarChartTitle
-                    chartInfo={props.chartInfo}
-                />
+                <BarChartTitle chartInfo={props.chartInfo} />
                 <Modal
                     // ix={ix}
                     // currencyInfo={currencyInfo}
@@ -35,8 +39,8 @@ import BarChartTitle from "../BarChartTitle/BarChartTitle";
                     {...props}
                 />
             </div>
-            </div>
-    )
- }
+        </div>
+    );
+}
 
- export default BarChart;
+export default BarChart;
